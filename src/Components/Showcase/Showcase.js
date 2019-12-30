c7abbcc14779bd155460a8db794b239dd6871e55
Kevin Montero <kevinwilding1997@gmail.com>
2019-12-30 00:19:28 -0700

    showcase done

---
+++ src/Components/Showcase/Showcase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import picture from '../../img/showcase.jpg'
+import './Showcase.sass'
+const Showcase =()=>{
+    return(
+        <div className="Showcase">
+            <img src={picture} alt=""/>
+            <div className="content ">
+                <h1>Aaron <br/>is all you need</h1>
+                <p>Simplicity, carried to an an extreme, becomes elegance.<br/> <span>- Jon Franklin</span></p>
+                <button>Shop now</button>
+            </div>
+        </div>
+    )
+}
+
+export default Showcase
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // import UpperLine from '../Components/UpperLine/UpperLine';
+import Showcase from '../Components/Showcase/Showcase'
 class Home extends Component {
 	state = {};
 	render() {
 		return (
 			<div className='Home'>
 				{/* <UpperLine /> */}
+				<Showcase />
 			</div>
 		);
 	}
