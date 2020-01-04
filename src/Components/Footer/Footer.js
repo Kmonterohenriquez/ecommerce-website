24e91edfa835a12781f36b46a7898b4765817940
Kevin Montero <kevinwilding1997@gmail.com>
2020-01-04 00:43:37 -0700

    footer component done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import './Footer.sass'
+import {Link} from 'react-router-dom'
+
 const Footer = () => {
     return (
         <div className='Footer'>
             <div className="grid-footer container">
-                <div className="box">
-                    <ul>
-                        <li>Return Policy</li>
-                        <li>Shipping FAQ</li>
-                        <li>Contact Us</li>
-                    </ul>
+                <div className="box-1">
+                    <h1>logo</h1>
                 </div>
-                <div className="box">
+                <div className="box-2">
                     <h1>BECOME AN INSIDER</h1>
                     <p>Promotions, new products, and sales. Directly to your inbox.</p>
                     <div className='email-input'>
-                        <input type="text"/><i className="far fa-envelope"></i>
+                        <i className="far fa-envelope"></i>
+                        <input type="text" placeholder='email@aaron.com'/>
+                        <button>Submit</button>
                     </div>
                 </div>
-                <div className="box">
-                    <h1>logo</h1>
+                <div className="box-3">
+                    <div className='align-end'>
+                        <ul>
+                            <li><Link>Ties</Link></li>
+                            <li><Link>Suits</Link></li>
+                            <li><Link>Belts</Link></li>
+                            <li><Link>Shirts</Link></li>
+                            <li><Link>Return Policy</Link></li>
+                            <li><Link to={'/licensing'}>Licensing</Link></li>
+                            <li><Link>Contact Us</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import UpperLine from '../Components/UpperLine/UpperLine';
 import Showcase from '../Components/Showcase/Showcase'
 import Products from '../Components/Products/Products';
-import {Link} from 'react-router-dom'
 import WhyAaron from '../Components/WhyAaron/WhyAaron'
 import NewReleases from '../Components/NewReleases/NewReleases'
 import Footer from '../Components/Footer/Footer'
@@ -17,7 +16,7 @@ class Home extends Component {
 				<WhyAaron/>
 				{/* <Products title='New Releases' /> */}
 				<NewReleases />
-				<Link to={'/licensing'}>Licensing</Link>
+				
 				<Footer />
 			</div>
 		);
