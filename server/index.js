7609a63c4d59131a1665f263f0d96866df0fe8f0
Kevin Montero <kevinwilding1997@gmail.com>
2020-01-04 22:59:10 -0700

    server setup

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express'),
+	massive = require('massive'),
+	session = require('express-session'),
+	{ SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env,
+	authCtrl = require('./controllers/authController'),
+	reviewCtrl = require('./controllers/reviewController'),
+	app = express();
+
+app.use(express.json());
+
+app.use(
+	session({
+		resave: false,
+		saveUninitialized: true,
+		secret: SESSION_SECRET,
+		cookie: { maxAge: 1000 * 60 * 60 * 24 }
+	})
+);
+
+massive(CONNECTION_STRING).then(db => {
+	app.set('db', db);
+	console.log('db connected');
+});
+
+//auth endpoints
+app.post('/auth/login', authCtrl.login);
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/logout', authCtrl.logout);
+app.get('/auth/userData', authCtrl.userData);
+
+const port = SERVER_PORT;
+app.listen(port, () => console.log(`Server running on port ${port}`));
