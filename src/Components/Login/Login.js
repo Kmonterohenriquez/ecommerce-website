e015fb7f36ea7943549aa384a423fa3026d75bff
Kevin Montero <kevinwilding1997@gmail.com>
2020-02-19 23:45:20 -0400

    new updates

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import './Login.sass'
-import login_img  from '../../img/login.jpg'
+import { Link } from 'react-router-dom';
+import './Login.sass';
+import login_img from '../../img/login.jpg';
 const Login = () => {
 	return (
 		<div className='Login'>
 			<div className='Login-container container'>
 				<form>
-                    <h1>Login</h1>
+					<h1>Login</h1>
 					<input type='email' name='email' placeholder='Example@aaron.com' />
 					<input type='password' name='password' placeholder='Password...' />
 					<button type='submit'>Login</button>
-                    <p>Not a member yet? <Link>Register</Link></p>
+					<p>
+						Not a member yet? <Link>Register</Link>
+					</p>
 				</form>
-                <img src={login_img} alt=""/>
+				<div className='image-container'>
+					<img src={login_img} alt='' />
+				</div>
 			</div>
 		</div>
 	);
