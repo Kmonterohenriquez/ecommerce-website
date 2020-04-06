07ecaf79791b22157f51b3863a86674350df651a
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-06 03:43:32 -0400

    table styled, buttons added on AdminCenter Component

--- src/pages/AdminCenter/AdminCenter.js
+++ src/pages/AdminCenter/AdminCenter.js
@@ -1,12 +1,84 @@
-import React from 'react'
-import './AdminCenter.sass'
+import React from 'react';
+import './AdminCenter.sass';
 
 const AdminCenter = () => {
-    return (
-        <div className='AdminCenter'>
-            
-        </div>
-    )
-}
+	return (
+		<div className='AdminCenter'>
+			<div className='AdminCenter-container sm-container'>
+                <div className='header-container'>
+                    <h1 className='title'>Admin Center</h1>
+                    <button className='add-btn'><i className="fas fa-plus"></i>Add New</button>
+                </div>
+                <table>
+                    <tr>
+                        <th>Product</th>
+                        <th>Qty</th>
+                        <th>Age</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>50</td>
+                        <td className='btn-col'><button className='btn delete-btn'>Delete</button></td>
+                        <td className='btn-col'><button className='btn edit-btn'>Edit</button></td>
+                    </tr>
+                    
+                    
+                </table>
+            </div>
+		</div>
+	);
+};
 
-export default AdminCenter
+export default AdminCenter;
