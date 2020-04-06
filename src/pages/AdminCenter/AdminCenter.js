30b09a8b102bb0d6d0e3945c1d9fa7fb58a28b18
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-06 02:37:52 -0400

    Just Starting AdminCenter component

---
+++ src/pages/AdminCenter/AdminCenter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AdminCenter.sass'
+
+const AdminCenter = () => {
+    return (
+        <div className='AdminCenter'>
+            
+        </div>
+    )
+}
+
+export default AdminCenter
