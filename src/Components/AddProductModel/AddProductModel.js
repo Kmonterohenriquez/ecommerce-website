62104f2dc00361b3614e23d9bff081d0b851e319
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-10 23:28:55 -0400

    working on AddProductModel component

--- src/Components/AddProductModel/AddProductModel.js
+++ src/Components/AddProductModel/AddProductModel.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from 'react';
+import './AddProductModel.sass';
 const AddProductModel = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return (
+		<div className='AddProductModel'>
+			<h1>Add Product</h1>
+			<hr />
+			<form action=''>
+				<img src='' alt='' />
+				<label htmlFor=''>Product Name</label>
+				<input type='text' placeholder='Enter a Name' />
+				<label>Description</label>
+				<textarea
+					name=''
+					id=''
+					cols='30'
+					rows='10'
+					placeholder='Briefly description of the product...'
+				></textarea>
+			</form>
+		</div>
+	);
+};
 
-export default AddProductModel
+export default AddProductModel;
