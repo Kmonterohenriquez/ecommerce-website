07c53c086fd86366d3be7b325234283508ff178d
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-13 23:11:07 -0400

    qty input added on AddProduct component

--- src/Components/AddProductModel/AddProductModel.js
+++ src/Components/AddProductModel/AddProductModel.js
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import './AddProductModel.sass';
 const AddProductModel = props => {
 	const [name, setName] = useState('');
-	const [qty, setQty] = useState(0);
+	const [qty, setQty] = useState(1);
 	const [description, setDescription] = useState('');
 
-	function showResult(){
-		console.clear()
-		console.log("Name: ", name)
-		console.log("qty: ", qty)
-		console.log("Description: ", description)
-		props.AddBtnToggle()
+	function handleQty(e) {
+		if (qty < 1) {
+			setQty(1)
+		} else {
+			setQty(e.target.value);
+		}
 	}
-	
+	function showResult() {
+		console.clear();
+		console.log('Name: ', name);
+		console.log('qty: ', qty);
+		console.log('Description: ', description);
+		props.AddBtnToggle();
+	}
+
 	return (
 		<div className='AddProductModel'>
 			<i className='close-icon fas fa-times' onClick={props.AddBtnToggle}></i>
@@ -25,9 +32,25 @@ const AddProductModel = props => {
 				</div>
 				<div className='right-side'>
 					<label htmlFor=''>Product Name</label>
-					<input type='text' placeholder='Enter a Name' onChange={(e)=> setName(e.target.value)}/>
+					<input
+						type='text'
+						placeholder='Enter a Name'
+						onChange={e => setName(e.target.value)}
+					/>
 					<label htmlFor=''>Quantity</label>
-					<input className='qty' type='number' placeholder='Enter quantity' onChange={(e)=> setQty(e.target.value)}/>
+					<input
+						className='qty'
+						type='number'
+						placeholder='Enter quantity'
+						onChange={e => handleQty(e)}
+						value= {qty}
+					/>
+					<select name="" id="">
+						<option value="">Choose a size</option>
+						<option value="s">Small</option>
+						<option value="m">Medium</option>
+						<option value="l">Large</option>
+					</select>
 					<label>Description</label>
 					<textarea
 						name=''
@@ -35,7 +58,7 @@ const AddProductModel = props => {
 						cols='30'
 						rows='10'
 						placeholder='Briefly description of the product...'
-						onChange={(e)=> setDescription(e.target.value)}
+						onChange={e => setDescription(e.target.value)}
 					></textarea>
 				</div>
 			</form>
@@ -43,7 +66,9 @@ const AddProductModel = props => {
 				<button className='btn cancel-btn' onClick={props.AddBtnToggle}>
 					Cancel
 				</button>
-				<button className='btn add-btn' onClick={showResult}>Add</button>
+				<button className='btn add-btn' onClick={showResult}>
+					Add
+				</button>
 			</div>
 		</div>
 	);
