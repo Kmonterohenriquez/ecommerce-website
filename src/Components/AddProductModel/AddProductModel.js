eba6e9b7ce51c3f917b691b74f6048d862fa2525
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-13 21:55:32 -0400

    get values from input in AddProductModel component

--- src/Components/AddProductModel/AddProductModel.js
+++ src/Components/AddProductModel/AddProductModel.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddProductModel.sass';
-const AddProductModel = (props) => {
+const AddProductModel = props => {
+	const [name, setName] = useState('');
+	const [qty, setQty] = useState(0);
+	const [description, setDescription] = useState('');
+
+	function showResult(){
+		console.clear()
+		console.log("Name: ", name)
+		console.log("qty: ", qty)
+		console.log("Description: ", description)
+		props.AddBtnToggle()
+	}
+	
 	return (
 		<div className='AddProductModel'>
-			<i className="close-icon fas fa-times" onClick={props.AddBtnToggle}></i>
+			<i className='close-icon fas fa-times' onClick={props.AddBtnToggle}></i>
 			<h1>Add Product</h1>
 			<hr />
 			<form action=''>
 				<div className='img-container'>
-					<i className="fas fa-plus"></i>
+					<i className='fas fa-plus'></i>
 					<img src='' alt='' />
 				</div>
 				<div className='right-side'>
 					<label htmlFor=''>Product Name</label>
-					<input type='text' placeholder='Enter a Name' />
+					<input type='text' placeholder='Enter a Name' onChange={(e)=> setName(e.target.value)}/>
 					<label htmlFor=''>Quantity</label>
-					<input type='text' placeholder='Enter quantity' />
+					<input className='qty' type='number' placeholder='Enter quantity' onChange={(e)=> setQty(e.target.value)}/>
 					<label>Description</label>
 					<textarea
 						name=''
@@ -23,12 +35,15 @@ const AddProductModel = (props) => {
 						cols='30'
 						rows='10'
 						placeholder='Briefly description of the product...'
+						onChange={(e)=> setDescription(e.target.value)}
 					></textarea>
 				</div>
 			</form>
-			<div className="btn-container">
-				<button className='btn cancel-btn' onClick={props.AddBtnToggle}>Cancel</button>
-				<button className='btn add-btn'>Add</button>
+			<div className='btn-container'>
+				<button className='btn cancel-btn' onClick={props.AddBtnToggle}>
+					Cancel
+				</button>
+				<button className='btn add-btn' onClick={showResult}>Add</button>
 			</div>
 		</div>
 	);
