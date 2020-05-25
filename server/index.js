0e317337f3c9efd72c6973ea24bc22cd5c0496ed
Kevin Montero <kevinwilding1997@gmail.com>
2020-05-24 23:54:29 -0400

    new changes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
 const express = require('express'),
 	massive = require('massive'),
-	bodyParser = require('body-parser'), 
-	session = require('express-session'),
+	// bodyParser = require('body-parser'), 
+	// session = require('express-session'),
 	{ SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env,
-	authCtrl = require('./controllers/authControllers'),
-	itemCtrl = require('./controllers/itemControllers');
+	// authCtrl = require('./controllers/authControllers'),
+	// itemCtrl = require('./controllers/itemControllers');
 app = express();
 
 app.use(express.json());
 
 
-app.use(
-	session({
-		resave: false,
-		saveUninitialized: true,
-		secret: SESSION_SECRET,
-		cookie: { maxAge: 1000 * 60 * 60 * 24 }
-	})
-);
+// app.use(
+// 	session({
+// 		resave: false,
+// 		saveUninitialized: true,
+// 		secret: SESSION_SECRET,
+// 		cookie: { maxAge: 1000 * 60 * 60 * 24 }
+// 	})
+// );
 
 massive(CONNECTION_STRING).then(db => {
 	app.set('db', db);
@@ -26,9 +26,9 @@ massive(CONNECTION_STRING).then(db => {
 });
 
 // AUTH ENDPOINTS
-app.post('/auth/login', authCtrl.login);
-app.post('/auth/register', authCtrl.register);
-app.post('/auth/logout', authCtrl.logout);
+// app.post('/auth/login', authCtrl.login);
+// app.post('/auth/register', authCtrl.register);
+// app.post('/auth/logout', authCtrl.logout);
 // app.get('/auth/userData', authCtrl.userData);
 // axios.get(`/api/items/name=${name}&description=${description}`)
 
--- src/Components/WhyAaron/WhyAaron.js
+++ src/Components/WhyAaron/WhyAaron.js
@@ -7,12 +7,12 @@ class WhyAaron extends Component {
         return (
             <div className="WhyAaron">
                 <div className=' center-container sm-container'>
+                    <div className='right-side'>
+                        <img src={why_aaron} alt=""/>
+                    </div>
                     <div className='left-side'>
                         <h1>Why Aaron?</h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sequi aliquam, quam officia labore dolorum quae voluptatibus. Harum rerum molestiae itaque cum, fugit sequi suscipit aliquid, officiis, perferendis illum necessitatibus.</p>
-                    </div>
-                    <div className='right-side'>
-                        <img src={why_aaron} alt=""/>
                     </div>
                 </div>
             </div>
